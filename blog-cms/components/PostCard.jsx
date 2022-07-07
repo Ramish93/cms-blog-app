d3e25c8629a129dfc332f815f98516d5e476d793
Ramish Hassan <ramishshah93@gmail.com>
2022-07-07 22:57:11 +0200

    card component completed

--- blog-cms/components/PostCard.jsx
+++ blog-cms/components/PostCard.jsx
@@ -10,7 +10,7 @@ const PostCard = ({ post }) => {
         <img
           src={post.featuredImage.url}
           alt={post.title}
-          className="object-top absolute h-80 w-full object-cover shadow-lg rounded-r-lg lg:rounded-lg"
+          className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"
         />
       </div>
       <h1
@@ -28,7 +28,37 @@ const PostCard = ({ post }) => {
             width="40px"
             className="align-middle rounded-full"
           />
+          <p className="inline align-middle text-gray-700 ml-2 text-lg">
+            {post.author.name}
+          </p>
         </div>
+        <div className="font-medium text-gray-700">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 inline mr-2 text-blue-500"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+            />
+          </svg>
+          <span>{moment(post.createdAt).format("MMM DD, YYYY")}</span>
+        </div>
+      </div>
+      <p className="text-center text-lg text-gray-750 font-normal px-4">
+        {post.excerpt}
+      </p>
+      <div className="text-center">
+        <Link href={`/post/${post.slug}`}>
+          <span className="transition duration-500 transform hover:-translate-y-1 inline-block bg-blue-700 text-lg font-medium rounded-full text-white px-5 py-2 cursor-pointer">
+            Continue Reading
+          </span>
+        </Link>
       </div>
     </div>
   );

a507b95df5e47f62bbd06302f22f00333bde39da
Ramish Hassan <ramishshah93@gmail.com>
2022-07-07 17:32:44 +0200

    header component complete, layout component ready, graphQl query for posts written in service folder

--- blog-cms/components/Header.jsx
+++ blog-cms/components/Header.jsx
@@ -2,14 +2,14 @@ import React, { useContext } from "react";
 import Link from "next/link";
 
 const categories = [
-  { name: "react CMs", slug: "React" },
-  { name: "web dev  CMS CMs", slug: "web-dev" },
+  { name: " react CMs", slug: "React" },
+  { name: "web dev CMS", slug: "web-dev" },
 ];
 
 const Header = () => {
   return (
     <div className="container mx-auto px-10 mb-8">
-      <div className="border-b w-full inline-block border-blue-400 py-8">
+      <div className="border-b w-full inline-block border-white-400 py-8">
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
@@ -19,10 +19,11 @@ const Header = () => {
         </div>
         <div className="hidden md:float-left md:contents">
           {categories.map((category) => (
-            <Link
-              key={category.slug}
-              href={`/category/${category.slug}`}
-            ></Link>
+            <Link key={category.slug} href={`/category/${category.slug}`}>
+              <span className="md:float-right mt-2 align-middle text-white mll-4 font-bold cursor-pointer">
+                {category.name}
+              </span>
+            </Link>
           ))}
         </div>
       </div>
--- blog-cms/pages/index.js
+++ blog-cms/pages/index.js
@@ -1,12 +1,8 @@
 import Head from "next/head";
 import { PostCard, PostWidget, Categories } from "../components";
+import { getPosts } from "../services";
 
-const posts = [
-  { title: "react testing", excerpt: "learn react testing" },
-  { title: "react tailwind", excerpt: "learn react tailwind" },
-];
-
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -29,4 +25,12 @@ export default function Home() {
       </div>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts },
+  };
 }
